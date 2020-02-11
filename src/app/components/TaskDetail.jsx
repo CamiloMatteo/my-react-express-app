6d37da7e9b2fb78fc7a07a60ef4c1db802a28032
CamiloMatteo <matteo.camilo21@gmail.com>
2020-02-11 19:39:22 -0300

    addig functional tasks with mutations (mock)

---
+++ src/app/components/TaskDetail.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import * as mutations from '../store/mutations'
+
+const TaskDetail = ({
+    id, comments, task, isComplete, groups, setTaskComplete, setTaskGroup, setTaskName
+}) => (
+    <div>
+        <div>
+            <h1>Task Detail</h1>
+        </div>
+        <div>
+            <input onChange={setTaskName} value={task.name} />
+        </div>
+        <div>
+            <button onClick={() => setTaskComplete(id, !isComplete)}>{isComplete ? `Reopen` : `Complete`}</button>
+        </div>
+        <div>
+            <select onChange={setTaskGroup} value={task.group}>
+                {groups.map(group => (
+                    <option key={group.id} value={group.id}>{group.name}</option>
+                ))}
+            </select>
+        </div>
+        <div>
+            <Link to="/dashborad">
+                <button>Done</button>
+            </Link>
+        </div>
+    </div>
+);
+
+// const mapStateToProps = state => state;
+const mapStateToProps = (state, ownProps) => {
+    let id = ownProps.match.params.id;
+    let task = state.tasks.find(task => task.id === id);
+    let groups = state.groups;
+
+    return{
+        id, task, groups, isComplete:task.isComplete
+    }
+}
+
+const mapDispathToProps = (dispath, ownProps) => {
+    const id = ownProps.match.params.id;
+    return {
+        setTaskComplete(id, isComplete){
+            dispath(mutations.setTaskComplete(id, isComplete));
+        },
+        setTaskGroup(e){
+            dispath(mutations.setTaskGroup(id, e.target.value));
+        },
+        setTaskName(e){
+            dispath(mutations.setTaskName(id, e.target.value));
+        }
+    }
+}
+
+export const ConnectTaskDetail = connect(mapStateToProps, mapDispathToProps)(TaskDetail);
--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { requestTaskCreation } from '../store/mutations';
+import { Link } from 'react-router-dom';
 
 export const TaskList = ({tasks, name, id, createNewTask}) => (
     <div>
@@ -8,9 +9,12 @@ export const TaskList = ({tasks, name, id, createNewTask}) => (
             {name}
         </h3>
         { tasks.map(task => (
-            <div key={ task.id }>
-                { task.name }
-            </div>
+            // <div key={ task.id }>
+            //     { task.name }
+            // </div>
+            <Link to={`/task/${task.id}`} key={task.id} >
+                <div>{task.name}</div>
+            </Link>
         )) }
         <button onClick={ () => createNewTask(id) }>Add New</button>
     </div>
